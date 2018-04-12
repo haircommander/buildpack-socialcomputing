72445ef76d8f5fd7abc4d32379c94e24aca73316
peterjh <peterjh@umich.edu>
2018-04-12 13:17:53 -0400

    added dependencies

---
+++ extra/proj_deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+npm install python-shell
+apt-get install python-pip
+pip install --upgrade pip
+pip install nltk
+python -m nltk.downloader stopwords
+python -m nltk.downloader wordnet
+python -m nltk.downloader averaged_preceptron_tagger
+python -m nltk.downloader punkt
